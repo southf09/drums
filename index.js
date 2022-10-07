7516e46ef510f26d09649d0d905031cff74cdff1
Frank South <frankstonesouth@gmail.com>
2022-10-07 16:54:22 -0400

    Small changes to JS script

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const buttons = document.querySelectorAll('.drum')
+const buttons = document.querySelectorAll('.drum');
+// const numberOfButtons = document.querySelectorAll('.drum').length;
 
 // for(let i = 0; i < buttons.length; i++){
 //     buttons[i].addEventListener('click', () => {
@@ -7,8 +8,20 @@ const buttons = document.querySelectorAll('.drum')
 // }
 
 for(let button of buttons){
-    button.addEventListener('click', () => {
-        let sound = new Audio('/sounds/crash.mp3');
-        sound.play();
+    button.addEventListener('click', function(){
+        // console.log(this.innerHTML);
+        this.classList.add('pressed');
     })
 }
+
+// for(let i = 0; i < numberOfButtons; i++){
+//     document.querySelectorAll('.drum')[i].addEventListener('click', () => {
+//         console.log(this);
+//     })
+// }
+
+// let sound = new Audio('/sounds/crash.mp3');
+//         sound.play();
+
+
+//Constructer function for a housekeeper
