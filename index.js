fca7ca17eda0d1cb5962ae6ddaa1d1763b156f33
Frank South <frankstonesouth@gmail.com>
2022-10-10 13:46:56 -0400

    Added a switch statement for each of the drums, with their corresponding sounds

--- index.js
+++ index.js
@@ -10,18 +10,50 @@ const buttons = document.querySelectorAll('.drum');
 for(let button of buttons){
     button.addEventListener('click', function(){
         // console.log(this.innerHTML);
-        this.classList.add('pressed');
+        // this.classList.add('pressed');
+
+        const buttonInnerHTML = this.innerHTML;
+        console.log(buttonInnerHTML);
+
+        switch (buttonInnerHTML) {
+            case 'w':
+                const tom1 = new Audio('sounds/tom-1.mp3')
+                tom1.play();
+                break;
+
+            case 'a':
+                const tom2 = new Audio('sounds/tom-2.mp3')
+                tom2.play();
+                break;
+
+            case 's':
+                const tom3 = new Audio('sounds/tom-3.mp3')
+                tom3.play();
+                break;
+
+            case 'd':
+                const tom4 = new Audio('sounds/tom-4.mp3')
+                tom4.play();
+                break;
+        
+            case 'j':
+                const crash = new Audio('sounds/crash.mp3')
+                crash.play();
+                break;
+
+            case 'k':
+                const kick = new Audio('sounds/kick-bass.mp3')
+                kick.play();
+                break;    
+
+            case 'l':
+                const snare = new Audio('sounds/snare.mp3')
+                snare.play();
+                break;
+
+            default:
+                break;
+        }
     })
 }
 
-// for(let i = 0; i < numberOfButtons; i++){
-//     document.querySelectorAll('.drum')[i].addEventListener('click', () => {
-//         console.log(this);
-//     })
-// }
-
-// let sound = new Audio('/sounds/crash.mp3');
-//         sound.play();
-
-
-//Constructer function for a housekeeper
