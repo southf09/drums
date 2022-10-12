5751fad5d3addc0bcd6fccd56b6c1ff385453010
Frank South <frankstonesouth@gmail.com>
2022-10-12 17:22:17 -0400

    Added an event listener for keydowns on the keyboard that plays the audio through a switch statement

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ for(let button of buttons){
 
         const buttonInnerHTML = this.innerHTML;
         console.log(buttonInnerHTML);
-
+        
         switch (buttonInnerHTML) {
             case 'w':
                 const tom1 = new Audio('sounds/tom-1.mp3')
@@ -49,4 +49,46 @@ for(let button of buttons){
         }
     })
 }
+
+window.addEventListener('keydown', function(e) {
+    switch (e.key) {
+        case 'w':
+            const tom1 = new Audio('sounds/tom-1.mp3')
+            tom1.play();
+            break;
+    
+        case 'a':
+            const tom2 = new Audio('sounds/tom-2.mp3')
+            tom2.play();
+            break;
+    
+        case 's':
+            const tom3 = new Audio('sounds/tom-3.mp3')
+            tom3.play();
+            break;
+    
+        case 'd':
+            const tom4 = new Audio('sounds/tom-4.mp3')
+            tom4.play();
+            break;
+    
+        case 'j':
+            const crash = new Audio('sounds/crash.mp3')
+            crash.play();
+            break;
+    
+        case 'k':
+            const kick = new Audio('sounds/kick-bass.mp3')
+            kick.play();
+            break;    
+    
+        case 'l':
+            const snare = new Audio('sounds/snare.mp3')
+            snare.play();
+            break;
+    
+        default:
+            break;
+    }
+})
 
